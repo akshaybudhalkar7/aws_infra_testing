0948c61ac18fd2b4f49280a6b3b0f7c4ca7e4830
unknown <akshaybudhalkar7@gmail.com>
2024-01-04 19:21:25 +0530

    added role

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     aws_s3 as _s3,
     Stack,
-   aws_lambda
+    aws_lambda,
+    aws_iam
 )
 from os import path
 
@@ -14,6 +15,15 @@ class S3Stack(Stack):
 
         # s3 = _s3.Bucket(self, "s3bucket6432889")
 
+        lambda_role = aws_iam.Role(
+            self, 'lambda_role',
+            assumed_by=aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+            managed_policies=[
+                aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaVPCAccessExecutionRole")
+                aws_iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchFullAccess")
+            ]
+        )
+
         # Defines an AWS Lambda resource
         my_lambda = aws_lambda.Function(
             self,
@@ -21,7 +31,8 @@ class S3Stack(Stack):
             runtime= aws_lambda.Runtime.PYTHON_3_7,
             code=aws_lambda.Code.from_asset(path.join(path.dirname(__file__), '..', '..', 'lambda_app')),
             handler='app.handler',
-            function_name='test_app'
+            function_name='test_app',
+            role= lambda_role
         )
 
 
