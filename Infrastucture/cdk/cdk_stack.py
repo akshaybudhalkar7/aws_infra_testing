3b6ba58128e03a55f4ec52cd82332b68f03bb603
unknown <akshaybudhalkar7@gmail.com>
2024-01-04 19:02:43 +0530

    added lambda

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     aws_s3 as _s3,
-    Stack
+    Stack,
+   aws_lambda
 )
 
 from constructs import Construct
@@ -10,5 +11,16 @@ class S3Stack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        s3 = _s3.Bucket(self, "s3bucket6432889")
+        # s3 = _s3.Bucket(self, "s3bucket6432889")
+
+        # Defines an AWS Lambda resource
+        my_lambda = aws_lambda.Function(
+            self,
+            'HelloHandler',
+            runtime= aws_lambda.Runtime.PYTHON_3_7,
+            code=aws_lambda.Code.from_asset(path.join(path.dirname(__file__),'..','..', 'lambda_app')),
+            handler='app.handler',
+            function_name='test_app'
+        )
+
 
